d295ac19df813af0ae5fc06a56ee8fef4877a516
saraswata tripathi <saraswata.tripathi2020@gmail.com>
2026-02-21 13:37:31 +0000

    now search bar is too small increse its width (_for element div_)

--- src/components/blocks/ClinicDetailsSection.tsx
+++ src/components/blocks/ClinicDetailsSection.tsx
@@ -87,7 +87,7 @@ export default function ClinicDetailsSection() {
                   <Input
                     type="search"
                     placeholder="Search by PIN..."
-                    className="w-full sm:w-[150px] rounded-lg bg-background pl-8"
+                    className="w-full sm:w-[200px] rounded-lg bg-background pl-8"
                     value={pinSearch}
                     onChange={(e) => setPinSearch(e.target.value)}
                   />
