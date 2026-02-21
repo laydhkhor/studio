cc3368d5ec39cccd5ade430f5dc541231a76d328
saraswata tripathi <saraswata.tripathi2020@gmail.com>
2026-02-21 14:11:19 +0000

    increce margine bottom little bit (_for element h2_)

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -15,7 +15,7 @@ export default function ContactPage() {
         </header>
 
         <div className="max-w-2xl mx-auto">
-            <h2 className="font-headline text-3xl font-semibold mb-6 text-center">Send a Message</h2>
+            <h2 className="font-headline text-3xl font-semibold mb-8 text-center">Send a Message</h2>
             <form className="space-y-4">
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div className="space-y-2">
