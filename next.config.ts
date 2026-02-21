3045dc197d16a0c5f1d1f49d3c0a538e09f46f6e
saraswata tripathi <saraswata.tripathi2020@gmail.com>
2026-02-21 12:29:39 +0000

    how many time to setting up service acc

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
@@ -23,7 +17,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
