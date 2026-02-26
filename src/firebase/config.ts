dae37065f330c23a34bea8bf33a4f821eda65b5d
saraswata tripathi <saraswata.tripathi2020@gmail.com>
2026-02-26 19:10:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9817865319-c37a1",
+  "appId": "1:314401596073:web:35ee442e5064bc2975bfb8",
+  "apiKey": "AIzaSyA7u5oD4Y5h89DfL19NugJmaBC86QDxWgc",
+  "authDomain": "studio-9817865319-c37a1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "314401596073"
+};
