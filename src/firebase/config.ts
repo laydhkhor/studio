5498cc97965928b83ab4de6c64f0a3aa95fe730f
saraswata tripathi <saraswata.tripathi2020@gmail.com>
2026-02-27 12:42:32 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
+  "projectId": "studio-9817865319-c37a1",
+  "appId": "1:314401596073:web:35ee442e5064bc2975bfb8",
   "apiKey": "AIzaSyA7u5oD4Y5h89DfL19NugJmaBC86QDxWgc",
   "authDomain": "studio-9817865319-c37a1.firebaseapp.com",
-  "projectId": "studio-9817865319-c37a1",
-  "storageBucket": "studio-9817865319-c37a1.firebasestorage.app",
-  "messagingSenderId": "314401596073",
-  "appId": "1:314401596073:web:35ee442e5064bc2975bfb8"
+  "measurementId": "",
+  "messagingSenderId": "314401596073"
 };
