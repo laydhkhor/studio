e1320bd01c6631de574794e760a91f6dfe859fd8
saraswata tripathi <saraswata.tripathi2020@gmail.com>
2026-02-28 20:27:53 +0000

    overflow problem is solved but public navigation is not sticky in to top

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
     )}>
       <body className={cn('min-h-screen bg-background font-body antialiased selection:bg-primary/10')}>
         <FirebaseClientProvider>
-          <div className="relative flex flex-col w-full overflow-x-hidden min-h-screen">
+          <div className="relative flex flex-col w-full min-h-screen">
             {children}
           </div>
         </FirebaseClientProvider>
