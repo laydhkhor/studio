ec860a963ddcf2919e00e29987a79c5e3ebaf020
saraswata tripathi <saraswata.tripathi2020@gmail.com>
2026-02-28 17:05:35 +0000

    sanity muted

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,26 +1,26 @@
-import {createClient} from 'next-sanity'
+// import {createClient} from 'next-sanity'
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION!
-export const token = process.env.SANITY_API_READ_TOKEN!
-export const previewSecret = process.env.SANITY_PREVIEW_SECRET!
+// export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+// export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+// export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION!
+// export const token = process.env.SANITY_API_READ_TOKEN!
+// export const previewSecret = process.env.SANITY_PREVIEW_SECRET!
 
-export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true,
-})
+// export const client = createClient({
+//   projectId,
+//   dataset,
+//   apiVersion,
+//   useCdn: true,
+// })
 
-export const previewClient = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: false,
-  token,
-})
+// export const previewClient = createClient({
+//   projectId,
+//   dataset,
+//   apiVersion,
+//   useCdn: false,
+//   token,
+// })
 
-export function getClient(preview: boolean = false) {
-  return preview ? previewClient : client
-}
+// export function getClient(preview: boolean = false) {
+//   return preview ? previewClient : client
+// }
